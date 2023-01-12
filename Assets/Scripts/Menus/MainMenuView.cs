8281ee74567969e32b861fdcc654e790f805414f
Callum Berger <callumBerger@hotmail.com>
2023-01-12 02:47:38 +0000

    Design Changes and Calendar

--- Assets/Scripts/Menus/MainMenuView.cs
+++ Assets/Scripts/Menus/MainMenuView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuView : View {
     [SerializeField] private Button _settingsButton;
@@ -10,6 +11,9 @@ public class MainMenuView : View {
     [SerializeField] private Button _journalButton;
     [SerializeField] private Button _checkInButton;
     [SerializeField] private Button _checkInAnalysisButton;
+    [SerializeField] private GameObject _calendarContent;
+
+    private GameObject _dateButton;
 
     public override void Initialise() {
         _settingsButton.onClick.AddListener(() => ViewManager.Show<SettingsMenuView>());
@@ -27,6 +31,10 @@ public class MainMenuView : View {
         _checkInButton.onClick.AddListener(() => ViewManager.Show<CheckInMenuView>());
 
         _checkInAnalysisButton.onClick.AddListener(() => ViewManager.Show<CheckInAnalysisMenuView>());
+
+        _dateButton = (GameObject)Resources.Load("Prefab/Menu/Date Button (TMP)");
+
+        CreateCalendarScrollView();
     }
 
     public override void Show() {
@@ -35,6 +43,85 @@ public class MainMenuView : View {
         if (AppManager.instance._uData.hasUserCheckedIn) {
             _checkInButton.GetComponent<Image>().color = Color.green;
             _checkInButton.enabled = false;
+        }
+    }
+
+    private void CreateCalendarScrollView() {
+        // 30 sept, apr, jun, nov
+        // 31
+        // 28/29 feb
+
+        int curDay = System.DateTime.Now.Day;
+        int curMonth = System.DateTime.Now.Month;
+        int curYear = System.DateTime.Now.Year;
+
+        int startMonth = curMonth - 1;
+        int startYear;
+
+        if (startMonth == 0) {
+            startMonth = 12;
+            startYear = curYear - 1;
+        } else {
+            startYear = curYear;
+        }
+
+        int startDaysInMonth = DaysInMonth(startMonth, startYear);
+
+        for (int i = 0; i < startDaysInMonth + 1; i++) {
+            GameObject newDate = Instantiate(_dateButton, _calendarContent.transform);
+            newDate.transform.GetChild(0).GetComponent<TMP_Text>().text = ConvertMonthToString(startMonth);
+            newDate.transform.GetChild(1).GetComponent<TMP_Text>().text = curDay.ToString();
+            curDay++;
+
+            if (curDay > startDaysInMonth) {
+                curDay = 1;
+                startMonth = curMonth;
+            }
+        }
+    }
+
+    private int DaysInMonth(int month, int year) {
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        } else if (month == 2) {
+            if (year % 4 == 0) {
+                return 29;
+            } else {
+                return 28;
+            }
+        } else {
+            return 31;
+        }
+    }
+
+    private string ConvertMonthToString(int month) {
+        switch (month) {
+            case 1:
+                return "Jan";
+            case 2:
+                return "Feb";
+            case 3:
+                return "Mar";
+            case 4:
+                return "Apr";
+            case 5:
+                return "May";
+            case 6:
+                return "Jun";
+            case 7:
+                return "Jul";
+            case 8:
+                return "Aug";
+            case 9:
+                return "Sep";
+            case 10:
+                return "Oct";
+            case 11:
+                return "Nov";
+            case 12:
+                return "Dec";
+            default:
+                return "";
         }
     }
 }
