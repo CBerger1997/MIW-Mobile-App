653b818cb5ce80aa1f5e1699e7d93670f7d5e98e
Callum Berger <callumBerger@hotmail.com>
2023-03-18 15:49:15 +0000

    FireBase Initialisation

--- Assets/Scripts/Managers/AppManager.cs
+++ Assets/Scripts/Managers/AppManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class AppManager : MonoBehaviour {
+public class AppManager : MonoBehaviour
+{
 
     public static AppManager instance = null;
 
@@ -12,15 +13,23 @@ public class AppManager : MonoBehaviour {
 
     public AffirmationManager _aManager { get; set; }
 
-    public static AppManager Instance {
+    private Firebase.FirebaseApp _fbApplication;
+    private bool _isFirebaseSet;
+
+    public static AppManager Instance
+    {
         get { return instance; }
     }
 
-    private void Awake() {
+    private void Awake()
+    {
         // Check for singleton instance and set or destory the instance accordingly
-        if (instance == null) {
+        if (instance == null)
+        {
             instance = this;
-        } else if (instance != this) {
+        }
+        else if (instance != this)
+        {
             Destroy(gameObject);
         }
 
@@ -39,21 +48,51 @@ public class AppManager : MonoBehaviour {
         _aManager.ReadRandom();
 
         _aManager.ReadSelection();
+
+        InitialiseFirebase();
     }
 
-    public void SaveUserData() {
+    private void InitialiseFirebase()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                _fbApplication = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Debug.Log("Firebase is set");
+                _isFirebaseSet = true;
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    public void SaveUserData()
+    {
         SaveAndLoadUserData.SaveUserData(_uData);
     }
 
-    public void LoadUserData() {
+    public void LoadUserData()
+    {
         _uData = SaveAndLoadUserData.LoadUserData();
     }
 
-    public void SaveTableData() {
+    public void SaveTableData()
+    {
         SaveAndLoadTableData.SaveTableData(_tData);
     }
 
-    public void LoadTableData() {
+    public void LoadTableData()
+    {
         _tData = SaveAndLoadTableData.LoadTableData();
     }
 
